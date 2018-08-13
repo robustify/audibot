3afb1eb41f8236ab543e87fd4f41212bade8cf25
Micho Radovnikovich <mradov@gmail.com>
2018-08-12 23:41:34 -0400

    Migrated version from existing private repository

---
+++ audibot_gazebo/include/audibot_gazebo/AudibotInterfacePlugin.h
@@ -0,0 +1,102 @@
+#ifndef AUDIBOTINTERFACEPLUGIN_H
+#define AUDIBOTINTERFACEPLUGIN_H
+
+#include <ros/ros.h>
+#include <std_msgs/Float64.h>
+#include <geometry_msgs/TwistStamped.h>
+
+#include <gazebo/common/Plugin.hh>
+#include <gazebo/physics/physics.hh>
+#include <tf/tf.h>
+#include <tf/transform_broadcaster.h>
+
+namespace gazebo {
+
+// Kinematics parameters
+#define AUDIBOT_STEERING_RATIO      17.3  // Ratio between steering wheel angle and tire angle
+#define AUDIBOT_LOCK_TO_LOCK_REVS   3.2   // Number of steering wheel turns to go from lock to lock
+#define AUDIBOT_MAX_STEER_ANGLE     (M_PI * AUDIBOT_LOCK_TO_LOCK_REVS / AUDIBOT_STEERING_RATIO)
+#define AUDIBOT_WHEELBASE           2.65  // Distance between front and rear axles
+#define AUDIBOT_TRACK_WIDTH         1.638 // Distance between front wheels
+
+// Drag parameters
+#define ROLLING_RESISTANCE_COEFF  0.01
+#define AERO_DRAG_COEFF           0.35
+#define GRAVITY_ACCEL             9.81
+#define VEHICLE_MASS              1700.0
+#define WHEEL_RADIUS              0.36
+#define MAX_BRAKE_TORQUE          3000.0
+
+class AudibotInterfacePlugin : public ModelPlugin {
+public:
+  AudibotInterfacePlugin();
+  virtual ~AudibotInterfacePlugin();
+
+protected:
+  virtual void Load(physics::ModelPtr model, sdf::ElementPtr sdf);
+  virtual void Reset();
+
+private:
+  void twistTimerCallback(const ros::TimerEvent& event);
+  void tfTimerCallback(const ros::TimerEvent& event);
+  void OnUpdate(const common::UpdateInfo& info);
+  void recvSteeringCmd(const std_msgs::Float64ConstPtr& msg);
+  void recvThrottleCmd(const std_msgs::Float64ConstPtr& msg);
+  void recvBrakeCmd(const std_msgs::Float64ConstPtr& msg);
+  void twistStateUpdate();
+  void driveUpdate();
+  void steeringUpdate(const common::UpdateInfo& info);
+  void dragUpdate();
+  void stopWheels();
+  void setAllWheelTorque(double torque);
+  void setRearWheelTorque(double torque);
+
+  ros::NodeHandle* n_;
+  ros::Publisher pub_twist_;
+  ros::Subscriber sub_steering_cmd_;
+  ros::Subscriber sub_throttle_cmd_;
+  ros::Subscriber sub_brake_cmd_;
+  ros::Subscriber sub_model_states_;
+  ros::Timer twist_timer_;
+  ros::Timer tf_timer_;
+
+  tf::TransformBroadcaster br_;
+  physics::ModelPtr model_;
+  geometry_msgs::Twist twist_;
+  bool rollover_;
+  ignition::math::Pose3d world_pose_;
+  event::ConnectionPtr update_connection_;
+  physics::JointPtr steer_fl_joint_;
+  physics::JointPtr steer_fr_joint_;
+  physics::JointPtr wheel_rl_joint_;
+  physics::JointPtr wheel_rr_joint_;
+  physics::JointPtr wheel_fl_joint_;
+  physics::JointPtr wheel_fr_joint_;
+  physics::LinkPtr footprint_link_;
+  common::Time last_update_time_;
+
+  // SDF parameters
+  std::string robot_name_;
+  bool pub_tf_;
+  double tf_freq_;
+
+  // Steering values
+  double right_angle_;
+  double left_angle_;
+  double target_angle_;
+  double current_steering_angle_;
+
+  // Brakes
+  double brake_cmd_;
+  ros::Time brake_stamp_;
+
+  // Throttle
+  double throttle_cmd_;
+  ros::Time throttle_stamp_;
+};
+
+GZ_REGISTER_MODEL_PLUGIN(AudibotInterfacePlugin)
+
+}
+
+#endif // AUDIBOTINTERFACEPLUGIN_H
---
+++ audibot_gazebo/src/AudibotInterfacePlugin.cpp
@@ -0,0 +1,239 @@
+#include <audibot_gazebo/AudibotInterfacePlugin.h>
+
+namespace gazebo {
+
+AudibotInterfacePlugin::AudibotInterfacePlugin() {
+  target_angle_ = 0.0;
+  brake_cmd_ = 0.0;
+  throttle_cmd_ = 0.0;
+  current_steering_angle_ = 0.0;
+  rollover_ = false;
+}
+
+void AudibotInterfacePlugin::Load(physics::ModelPtr model, sdf::ElementPtr sdf) {
+  // Gazebo initialization
+  steer_fl_joint_ = model->GetJoint("steer_fl");
+  steer_fr_joint_ = model->GetJoint("steer_fr");
+  wheel_rl_joint_ = model->GetJoint("wheel_rl");
+  wheel_rr_joint_ = model->GetJoint("wheel_rr");
+  wheel_fl_joint_ = model->GetJoint("wheel_fl");
+  wheel_fr_joint_ = model->GetJoint("wheel_fr");
+  footprint_link_ = model->GetLink("base_footprint");
+  model_ = model;
+
+  // Load SDF parameters
+  if (sdf->HasElement("pubTf")) {
+    sdf->GetElement("pubTf")->GetValue()->Get(pub_tf_);
+  } else {
+    pub_tf_ = false;
+  }
+
+  if (sdf->HasElement("robotName")) {
+    sdf::ParamPtr sdf_robot_name = sdf->GetElement("robotName")->GetValue();
+    if (sdf_robot_name) {
+      sdf_robot_name->Get(robot_name_);
+    } else {
+      robot_name_ = std::string("");
+    }
+  } else {
+    robot_name_ = std::string("");
+  }
+
+  if (sdf->HasElement("pubTf")) {
+    sdf->GetElement("pubTf")->GetValue()->Get(pub_tf_);
+  } else {
+    pub_tf_ = false;
+  }
+
+  if (sdf->HasElement("tfFreq")) {
+    sdf->GetElement("tfFreq")->GetValue()->Get(tf_freq_);
+  } else {
+    tf_freq_ = 100.0;
+  }
+
+  update_connection_ = event::Events::ConnectWorldUpdateBegin(boost::bind(&AudibotInterfacePlugin::OnUpdate, this, _1));
+
+  steer_fl_joint_->SetParam("fmax", 0, 99999.0);
+  steer_fr_joint_->SetParam("fmax", 0, 99999.0);
+
+  // ROS initialization
+  n_ = new ros::NodeHandle(robot_name_);
+
+  sub_steering_cmd_ = n_->subscribe("steering_cmd", 1, &AudibotInterfacePlugin::recvSteeringCmd, this);
+  sub_brake_cmd_ = n_->subscribe("brake_cmd", 1, &AudibotInterfacePlugin::recvBrakeCmd, this);
+  sub_throttle_cmd_ = n_->subscribe("throttle_cmd", 1, &AudibotInterfacePlugin::recvThrottleCmd, this);
+
+  pub_twist_ = n_->advertise<geometry_msgs::TwistStamped> ("twist", 1);
+  twist_timer_ = n_->createTimer(ros::Duration(0.02), &AudibotInterfacePlugin::twistTimerCallback, this);
+  tf_timer_ = n_->createTimer(ros::Duration(1.0 / tf_freq_), &AudibotInterfacePlugin::tfTimerCallback, this);
+}
+
+void AudibotInterfacePlugin::OnUpdate(const common::UpdateInfo& info) {
+  if (last_update_time_ == common::Time(0)) {
+    last_update_time_ = info.simTime;
+    return;
+  }
+
+  twistStateUpdate();
+  driveUpdate();
+  steeringUpdate(info);
+  dragUpdate();
+}
+
+void AudibotInterfacePlugin::twistStateUpdate() {
+  world_pose_ = footprint_link_->WorldPose();
+
+  twist_.linear.x = footprint_link_->RelativeLinearVel().X();
+  twist_.angular.z = footprint_link_->RelativeAngularVel().Z();
+  rollover_ = (fabs(world_pose_.Rot().X()) > 0.2 || fabs(world_pose_.Rot().Y()) > 0.2);
+}
+
+void AudibotInterfacePlugin::driveUpdate() {
+  // Stop wheels if vehicle is rolled over
+  if (rollover_) {
+    stopWheels();
+    return;
+  }
+
+  // Brakes have precedence over throttle
+  ros::Time current_stamp = ros::Time::now();
+  if ((brake_cmd_ > 0) && ((current_stamp - brake_stamp_).toSec() < 0.25)) {
+    double brake_torque_factor = 1.0;
+    if (twist_.linear.x < -0.1) {
+      brake_torque_factor = -1.0;
+    } else if (twist_.linear.x < 0.1) {
+      brake_torque_factor = 1.0 + (twist_.linear.x - 0.1) / 0.1;
+    }
+
+    setAllWheelTorque(-brake_torque_factor * brake_cmd_);
+  } else {
+    if ((current_stamp - throttle_stamp_).toSec() < 0.25) {
+      double max_throttle_torque = throttle_cmd_ * 4000.0 - 40.1 * twist_.linear.x;
+      if (max_throttle_torque < 0.0) {
+        max_throttle_torque = 0.0;
+      }
+      setRearWheelTorque(max_throttle_torque);
+    }
+  }
+}
+
+void AudibotInterfacePlugin::steeringUpdate(const common::UpdateInfo& info) {
+  double time_step = (info.simTime - last_update_time_).Double();
+  last_update_time_ = info.simTime;
+
+  // Arbitrarily set maximum steering rate to 800 deg/s
+  const double max_rate = 800.0 * M_PI / 180.0 / AUDIBOT_STEERING_RATIO;
+  double max_inc = time_step * max_rate;
+
+  if ((target_angle_ - current_steering_angle_) > max_inc) {
+    current_steering_angle_ += max_inc;
+  } else if ((target_angle_ - current_steering_angle_) < -max_inc) {
+    current_steering_angle_ -= max_inc;
+  }
+
+  // Compute Ackermann steering angles for each wheel
+  double t_alph = tan(current_steering_angle_);
+  double left_steer = atan(AUDIBOT_WHEELBASE * t_alph / (AUDIBOT_WHEELBASE - 0.5 * AUDIBOT_TRACK_WIDTH * t_alph));
+  double right_steer = atan(AUDIBOT_WHEELBASE * t_alph / (AUDIBOT_WHEELBASE + 0.5 * AUDIBOT_TRACK_WIDTH * t_alph));
+
+  steer_fl_joint_->SetParam("vel", 0, 100.0 * (left_steer - steer_fl_joint_->Position(0)));
+  steer_fr_joint_->SetParam("vel", 0, 100.0 * (right_steer - steer_fr_joint_->Position(0)));
+
+}
+
+void AudibotInterfacePlugin::dragUpdate() {
+  // Apply rolling resistance and aerodynamic drag forces
+  double rolling_resistance_torque = ROLLING_RESISTANCE_COEFF * VEHICLE_MASS * GRAVITY_ACCEL;
+  double drag_force = AERO_DRAG_COEFF * twist_.linear.x * twist_.linear.x;
+  double drag_torque = drag_force * WHEEL_RADIUS; // Implement aerodynamic drag as a torque disturbance
+
+  if (twist_.linear.x > 0.0) {
+    setAllWheelTorque(-rolling_resistance_torque);
+    setAllWheelTorque(-drag_torque);
+  } else {
+    setAllWheelTorque(rolling_resistance_torque);
+    setAllWheelTorque(drag_torque);
+  }
+}
+
+void AudibotInterfacePlugin::setAllWheelTorque(double torque) {
+  wheel_rl_joint_->SetForce(0, 0.25 * torque);
+  wheel_rr_joint_->SetForce(0, 0.25 * torque);
+  wheel_fl_joint_->SetForce(0, 0.25 * torque);
+  wheel_fr_joint_->SetForce(0, 0.25 * torque);
+}
+
+void AudibotInterfacePlugin::setRearWheelTorque(double torque) {
+  wheel_rl_joint_->SetForce(0, 0.5 * torque);
+  wheel_rr_joint_->SetForce(0, 0.5 * torque);
+}
+
+void AudibotInterfacePlugin::stopWheels() {
+  wheel_fl_joint_->SetForce(0, -1000.0 * wheel_fl_joint_->GetVelocity(0));
+  wheel_fr_joint_->SetForce(0, -1000.0 * wheel_fr_joint_->GetVelocity(0));
+  wheel_rl_joint_->SetForce(0, -1000.0 * wheel_rl_joint_->GetVelocity(0));
+  wheel_rr_joint_->SetForce(0, -1000.0 * wheel_rr_joint_->GetVelocity(0));
+}
+
+void AudibotInterfacePlugin::recvSteeringCmd(const std_msgs::Float64ConstPtr& msg) {
+  if (!std::isfinite(msg->data)) {
+    target_angle_ = 0.0;
+    return;
+  }
+
+  target_angle_ = msg->data / AUDIBOT_STEERING_RATIO;
+  if (target_angle_ > AUDIBOT_MAX_STEER_ANGLE) {
+    target_angle_ = AUDIBOT_MAX_STEER_ANGLE;
+  } else if (target_angle_ < -AUDIBOT_MAX_STEER_ANGLE) {
+    target_angle_ = -AUDIBOT_MAX_STEER_ANGLE;
+  }
+}
+
+void AudibotInterfacePlugin::recvBrakeCmd(const std_msgs::Float64ConstPtr& msg) {
+  brake_cmd_ = msg->data;
+  if (brake_cmd_ < 0) {
+    brake_cmd_ = 0;
+  } else if (brake_cmd_ > MAX_BRAKE_TORQUE) {
+    brake_cmd_ = MAX_BRAKE_TORQUE;
+  }
+  brake_stamp_ = ros::Time::now();
+}
+
+void AudibotInterfacePlugin::recvThrottleCmd(const std_msgs::Float64ConstPtr& msg) {
+  throttle_cmd_ = msg->data;
+  if (throttle_cmd_ < 0.0) {
+    throttle_cmd_ = 0.0;
+  } else if (throttle_cmd_ > 1.0) {
+    throttle_cmd_ = 1.0;
+  }
+  throttle_stamp_ = ros::Time::now();
+}
+
+void AudibotInterfacePlugin::twistTimerCallback(const ros::TimerEvent& event) {
+  geometry_msgs::TwistStamped twist_msg;
+  twist_msg.header.frame_id = model_->GetName() + "/base_footprint";
+  twist_msg.header.stamp = event.current_real;
+  twist_msg.twist = twist_;
+  pub_twist_.publish(twist_msg);
+}
+
+void AudibotInterfacePlugin::tfTimerCallback(const ros::TimerEvent& event) {
+  tf::StampedTransform t;
+  t.frame_id_ = "world";
+  t.child_frame_id_ = tf::resolve(robot_name_, footprint_link_->GetName());
+  t.stamp_ = event.current_real;
+  t.setOrigin(tf::Vector3(world_pose_.Pos().X(), world_pose_.Pos().Y(), world_pose_.Pos().Z()));
+  t.setRotation(tf::Quaternion(world_pose_.Rot().X(), world_pose_.Rot().Y(), world_pose_.Rot().Z(), world_pose_.Rot().W()));
+  br_.sendTransform(t);
+}
+
+void AudibotInterfacePlugin::Reset() {
+}
+
+AudibotInterfacePlugin::~AudibotInterfacePlugin() {
+  n_->shutdown();
+  delete n_;
+}
+
+
+}
